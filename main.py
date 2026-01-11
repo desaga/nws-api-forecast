360bb0e9cbf85a5865d37511decefac093db99d3
Viktor Kuznietsov <desaga@gmail.com>
2026-01-11 16:26:35 -0600

    updated app.run

--- main.py
+++ main.py
@@ -96,4 +96,4 @@ def index():
                            lat=lat, lon=lon, location_name=location_name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
